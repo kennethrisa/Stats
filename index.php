b16d96455f8c6f5cba3ef9a78a5e3171e9c362f5
kennethrisa <kenneth@k-risa.no>
2017-02-13 15:59:20 +0100

    Added footer to index.php Updated stats1.php to have same size in fa-fa on server rank.

--- index.php
+++ index.php
@@ -3,48 +3,47 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <title>Altirust.no - Stats</title>
+<title>Altirust.no - Stats</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" />
-    <!-- Custom CSS -->
-    <link href="css/scrolling-nav.css" rel="stylesheet">
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" />
+<!-- Custom CSS -->
+<link href="css/scrolling-nav.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
 <?php include_once("../analyticstracking.php") ?>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
-  <!-- Navigation -->
-  <?php include("navbar.php"); ?>
-
-    <!-- Intro Section -->
-    <?php include("stats1.php"); ?>
-
-    <!-- jQuery -->
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Scrolling Nav JavaScript -->
-
-    <script src="js/scrolling-nav.js"></script>
-
+<div>
+<!-- Navigation -->
+<?php include("navbar.php"); ?>
+<!-- Intro Section -->
+<?php include("stats1.php"); ?>
+<!-- FOOTER -->
+    <footer>
+      <br />
+      <p class="pull-right"><a href="#"><i class="fa fa-angle-double-up fa-2x" title="Back to top"></i></a></p>
+      <p class="text-center">&copy; 2017. &middot; <a href="#">Altirust.no</a> &middot; <a href="https://github.com/kennethrisa/Stats">Want this on your site? check our <i class="fa fa-github"></i></a></p>
+    </footer>
+</div>
+<!-- jQuery -->
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Scrolling Nav JavaScript -->
+<script src="js/scrolling-nav.js"></script>
 </body>
-
 </html>
