22566c7110a832e8bd4bf107223dbbb6fe106380
kennethrisa <kenneth@k-risa.no>
2018-07-15 15:38:31 +0200

    Upgrade bootstrap

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" />
     <!-- Custom CSS -->
     <link href="css/scrolling-nav.css" rel="stylesheet">
