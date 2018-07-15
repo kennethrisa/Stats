4e64fd82e23377815f7dbd319c13e33807995ef0
kennethrisa <kenneth@k-risa.no>
2018-07-15 15:42:39 +0200

    * Added top players (1st, 2nd, 3rd) * Added top clan (1st, 2nd, 3rd) * Removed some top stats

--- servers/server1.php
+++ servers/server1.php
@@ -27,6 +27,8 @@ function cache_output( $content ) {
 }
 ?>
 <link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Upgrade to bootstrap 4.1.2 -->
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous"> -->
 <link href="//cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css" rel="stylesheet">
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script src="//cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
@@ -37,39 +39,53 @@ include("api/api-server1.php");
 ?>
 <?php
 
-$sqlTopHeli = "SELECT Name, HeliKills FROM playerranksdb
-where HeliKills > 0
-ORDER BY HeliKills desc limit 1";
+$sqlTopPlayer = "SELECT Name, PVPKills as PVPKills FROM playerranksdb
+order by PVPKills desc limit 1 offset 0";
 
-$sqlTopHeliHits = "SELECT Name, HeliHits FROM playerranksdb
-where HeliHits > 0
-ORDER BY HeliHits desc limit 1";
+$sqlTopPlayer2 = "SELECT Name, PVPKills as PVPKills FROM playerranksdb
+order by PVPKills desc limit 1 offset 1";
 
-$sqlTopStructuresBuilt = "SELECT Name, StructuresBuilt FROM playerranksdb
-ORDER BY StructuresBuilt desc limit 1";
+$sqlTopPlayer3 = "SELECT Name, PVPKills as PVPKills FROM playerranksdb
+order by PVPKills desc limit 1 offset 2";
 
 $sqlTopOnline = "SELECT Name, TimePlayed FROM playerranksdb
 ORDER BY TimePlayed desc limit 1";
 
-$result = $conn->query($sqlTopHeli);
-$data = $result->fetch_assoc();
-$heliPlayer =  $data['Name'];
-$heliTotalKills =  $data['HeliKills'];
+$sqlTopClan = "SELECT `Clan`, SUM(PVPKills) as PVPKills FROM `playerranksdb` where Clan not like 'None' group by `Clan` order by 2 desc limit 1";
+$sqlTopClan2 = "SELECT `Clan`, SUM(PVPKills) as PVPKills FROM `playerranksdb` where Clan not like 'None' group by `Clan` order by 2 desc limit 1 OFFSET 1";
+$sqlTopClan3 = "SELECT `Clan`, SUM(PVPKills) as PVPKills FROM `playerranksdb` where Clan not like 'None' group by `Clan` order by 2 desc limit 1 OFFSET 2";
 
-$result = $conn->query($sqlTopHeliHits);
+$result = $conn->query($sqlTopPlayer);
 $data = $result->fetch_assoc();
-$heliHitsName =  $data['Name'];
-$heliHitsTotal =  $data['HeliHits'];
+$topPlayer =  $data['Name'];
+$topPlayerKills =  $data['PVPKills'];
 
-$result = $conn->query($sqlTopStructuresBuilt);
+$result = $conn->query($sqlTopPlayer2);
 $data = $result->fetch_assoc();
-$pname =  $data['Name'];
-$StructuresBuilt =  $data['StructuresBuilt'];
+$topPlayer2 =  $data['Name'];
+$topPlayerKills2 =  $data['PVPKills'];
+
+$result = $conn->query($sqlTopPlayer3);
+$data = $result->fetch_assoc();
+$topPlayer3 =  $data['Name'];
+$topPlayerKills3 =  $data['PVPKills'];
 
 $result = $conn->query($sqlTopOnline);
 $data = $result->fetch_assoc();
 $timePlayedName =  $data['Name'];
 $timePlayeTotal =  $data['TimePlayed'];
+
+$result = $conn->query($sqlTopClan);
+$data = $result->fetch_assoc();
+$TopClan = $data['Clan'];
+
+$result = $conn->query($sqlTopClan2);
+$data = $result->fetch_assoc();
+$TopClan2 = $data['Clan'];
+
+$result = $conn->query($sqlTopClan3);
+$data = $result->fetch_assoc();
+$TopClan3 = $data['Clan'];
 
 ?>
 
@@ -127,47 +143,39 @@ $timePlayeTotal =  $data['TimePlayed'];
 <!--end row 1-->
 <!--row 2-->
 <div class="row">
-          <div class="col-md-3 col-sm-6">
+    <h3>
+          Top Players
+    </h3>
+          <div class="col-md-4 col-sm-6">
               <div class="card card-inverse bg-faded">
                   <div class="card-block bg-faded">
                       <div class="rotate">
                           <i class="fa fa-user fa-5x"></i>
                       </div>
-                      <h6 class="text-uppercase">Top heli-kills</h6>
-				<h4 class="display-6"><?php if ($heliTotalKills < "1") { echo "No data";} else { echo $heliPlayer; echo " <span class='badge' title='Total amount kills with this weapon'>$heliTotalKills</span>"; }?></h4>
+                      <h4 class="text font-weight-bold">1st</h4>
+				<h4 class="display-6"><?php if ($topPlayerKills < "1") { echo "No data";} else { echo $topPlayer; }?></h4>
                   </div>
               </div>
           </div>
-          <div class="col-md-3 col-sm-6">
+          <div class="col-md-4 col-sm-6">
 						<div class="card card-inverse bg-faded">
 								<div class="card-block bg-faded">
                       <div class="rotate">
                           <i class="fa fa-list fa-5x"></i>
                       </div><!-- Total users in database -->
-                      <h6 class="text-uppercase">Top heli-hits</h6>
-				<h4 class="display-6"><?php if ($heliHitsTotal < "1") { echo "No data";} else { echo $heliHitsName; echo " <span class='badge' title='Where players have most hits'>$heliHitsTotal</span>"; }?></h4>
+                      <h5 class="text font-weight-bold">2nd</h5>
+                      <h4 class="display-6"><?php if ($topPlayerKills2 < "1") { echo "No data";} else { echo $topPlayer2; }?></h4>
                   </div>
               </div>
           </div>
-          <div class="col-md-3 col-sm-6">
+          <div class="col-md-4 col-sm-6">
 						<div class="card card-inverse bg-faded">
 								<div class="card-block bg-faded">
                       <div class="rotate">
                           <i class="fa fa-deviantart fa-5x"></i>
                       </div>
-                      <h6 class="text-uppercase">Top StructuresBuilt</h6>
-                      <h4 class="display-6"><?php echo $pname; echo '<br>'; echo " <span class='badge' title='Total amount of kills with this weapon'>$StructuresBuilt</span>"; ?></h4>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-3 col-sm-6">
-						<div class="card card-inverse bg-faded">
-								<div class="card-block bg-faded">
-                      <div class="rotate">
-                          <i class="fa fa-share fa-5x"></i>
-                      </div>
-                      <h6 class="text-uppercase">Top time played</h6>
-                      <h4 class="display-7"><?php echo $timePlayedName; echo " <span class='badge' title='Total amount of time played'>$timePlayeTotal</span>"; ?></h4>
+                      <h5 class="text font-weight-bold">3rd</h5>
+                      <h4 class="display-6"><?php if ($topPlayerKills3 < "1") { echo "No data";} else { echo $topPlayer3; }?></h4>
                   </div>
               </div>
           </div>
@@ -418,11 +426,103 @@ echo "</div>";
   echo "</table>";
   echo "</div>";
   }
-$conn->close();
   ?>
 </div>
 </div>
   <!-- end row -->
+
+<!--row 3-->
+<div class="row">
+<h3>
+          Top Clans
+      </h3>
+          <div class="col-md-4 col-sm-6">
+              <div class="card card-inverse bg-faded">
+                  <div class="card-block bg-faded">
+                      <div class="rotate">
+                          <i class="fa fa-user fa-5x"></i>
+                      </div>
+                      <h4 class="text font-weight-bold">1st</h4>
+				<h4 class="display-6"><?php if ($TopClan < "1") { echo "No data";} else { echo $TopClan; }?></h4>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+						<div class="card card-inverse bg-faded">
+								<div class="card-block bg-faded">
+                      <div class="rotate">
+                          <i class="fa fa-list fa-5x"></i>
+                      </div><!-- Total users in database -->
+                      <h5 class="text font-weight-bold">2nd</h5>
+                      <h4 class="display-6"><?php if ($TopClan < "1") { echo "No data";} else { echo $TopClan2; }?></h4>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+						<div class="card card-inverse bg-faded">
+								<div class="card-block bg-faded">
+                      <div class="rotate">
+                          <i class="fa fa-deviantart fa-5x"></i>
+                      </div>
+                      <h5 class="text font-weight-bold">3rd</h5>
+                      <h4 class="display-6"><?php if ($TopClan < "1") { echo "No data";} else { echo $TopClan3; }?></h4>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- end row 3-->
+
+  <div class="row">
+  <div class="col-lg-12">
+      <h3>
+          Clans
+      </h3>
+      <?php
+  $sql = "SELECT `Clan`, SUM(PVPKills) as PVPKills, SUM(Deaths) as Deaths, SUM(KDR) as KDR  FROM `playerranksdb` where Clan not like 'None' group by `Clan` order by 2 desc limit 100";
+
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+  // output data of each row
+  echo " <div class='table-responsive'>";
+  echo "";
+  echo "";
+  echo "<table id='clans' class='display compact table table-hover table-striped results '>";
+  echo "<thead>";
+  echo "<tr>";
+  echo "<th>#</th>";
+  echo "<th>Clan</th>";
+  echo "<th>PVPKills</th>";
+  echo "<th>Deaths</th>";
+  echo "<th>KDR</th>";
+  echo "</tr>";
+  echo "</thead>";
+  echo "<tbody>";
+
+  $counter = 1;
+
+  while($row = $result->fetch_assoc()) {
+
+  $counter2 = $counter++;
+
+  echo "<tr>";
+  echo "<th scope='row'>$counter2</th>";
+  echo "<td>" . $row['Clan']. "</td>";
+  echo "<td>" . $row['PVPKills']. "</td>";
+  echo "<td>" . $row['Deaths']. "</td>";
+  echo "<td>" . $row['KDR']. "</td>";
+  echo "</tr>";
+
+  }
+  echo "</tbody>";
+  echo "</table>";
+  echo "</div>";
+  }
+$conn->close();
+  ?>
+</div>
+</div>
+  <!-- end row -->  
 <script>
 // Disable search and ordering by default
 $.extend( $.fn.dataTable.defaults, {
@@ -463,6 +563,12 @@ $(document).ready(function() {
 } );
 $(document).ready(function() {
     $('#example5').DataTable({
+	ordering: true
+	})
+} );
+
+$(document).ready(function() {
+    $('#clans').DataTable({
 	ordering: true
 	})
 } );
