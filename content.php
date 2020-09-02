4efbd46275508323c4334a3a5e326b7375df5dc2
P4R4NORM4L <belanger2008@gmail.com>
2020-09-02 11:27:14 -0600

    Added support for the 6 servers

--- content.php
+++ content.php
@@ -6,10 +6,26 @@ case "1":
     @include("servers/server1.php");
     break;
 
-// Uncomment below if you are gonna have multuple servers, and copy paste fore more
-// case "2":
-//     @include("servers/server2.php");
-//     break;
+case "2":
+     @include("servers/server2.php");
+     break;
+
+case "3":
+     @include("servers/server3.php");
+     break;
+
+case "4":
+    @include("servers/server4.php");
+    break;
+
+case "5":
+     @include("servers/server5.php");
+     break;
+
+case "6":
+     @include("servers/server6.php");
+     break;
+
 
 default:
     @include("servers/server1.php");
