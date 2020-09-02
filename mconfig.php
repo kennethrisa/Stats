5bf8bc8c8bbb5d09e4538f97f368e2a91a69b069
P4R4NORM4L <belanger2008@gmail.com>
2020-09-02 17:54:50 -0600

    Commented out servers 2-6, SQL Stats on server 1

--- mconfig.php
+++ mconfig.php
@@ -25,7 +25,8 @@ if ($conn_ply_rnk_1->connect_error) {
 }
 
 // Server 1 - SQLStats - 1 
-
+// Remove line 29 and 44 to use SQL Stats DB
+/*
 $servername_sql_stats_1 = "localhost";
 $port_sql_stats_1 = "3306";
 $username_sql_stats_1 = "SQLRanksUsername1";
@@ -36,11 +37,11 @@ $dbname_sql_stats_1 = "SQLRanksDatabaseName1";
 
 // Create connection
 $conn_sql_stats_1 = new mysqli($servername_sql_stats_1, $username_sql_stats_1, $password_sql_stats_1, $dbname_sql_stats_1, $port_sql_stats_1);
-// Check connection
-if ($conn_sql_stats_1->connect_error) {
-    die("Connection failed: " . $conn_sql_stats_1->connect_error);
-}
 
+if ($conn_sql_stats_1->connect_error) {
+   die("Connection failed: " . $conn_sql_stats_1->connect_error);
+}
+*/
 // Manual settings for Stats Reset
 $srv1LastReset = "<b>Last stats reset time:</b> 08/20";
 
@@ -48,11 +49,15 @@ $srv1LastReset = "<b>Last stats reset time:</b> 08/20";
 
 //-----------------------------------------------------------------------------------------
 
+// Remove lines 58 and 94 to use SERVER 2
+
 // Start of Server 2 Database configs
 
 // Server 2 - playerRanks - 2
+
 // Theses settings must match your playerRanks plugin config
 
+/*
 $servername_ply_rnk_2 = "localhost";
 $port_ply_rnk_2 = "3306";
 $username_ply_rnk_2 = "playerRanksdb2";
@@ -61,9 +66,9 @@ $dbname_ply_rnk_2 = "playerRanksdb2";
 
 // Connection - PlayerRanks - 2
 
-// Create connection
+
 $conn_ply_rnk_2 = new mysqli($servername_ply_rnk_2, $username_ply_rnk_2, $password_ply_rnk_2, $dbname_ply_rnk_2, $port_ply_rnk_2);
-// Check connection
+
 if ($conn_ply_rnk_2->connect_error) {
     die("Connection failed: " . $conn_ply_rnk_2->connect_error);
 }
@@ -78,25 +83,26 @@ $dbname_sql_stats_2 = "SQLRanksDatabaseName2";
 
 // Connection - SQLStats - 2
 
-// Create connection
-$conn_sql_stats_2 = new mysqli($servername_sql_stats_2, $username_sql_stats_2, $password_sql_stats_2, $dbname_sql_stats_2, $port_sql_stats_2);
-// Check connection
-if ($conn_sql_stats_2->connect_error) {
-    die("Connection failed: " . $conn_sql_stats_2->connect_error);
-}
+ $conn_sql_stats_2 = new mysqli($servername_sql_stats_2, $username_sql_stats_2, $password_sql_stats_2, $dbname_sql_stats_2, $port_sql_stats_2);
+
+ if ($conn_sql_stats_2->connect_error) {
+     die("Connection failed: " . $conn_sql_stats_2->connect_error);
+ }
 
 // Manual settings for Stats Reset
 $srv2LastReset = "<b>Last stats reset time:</b> 08/20";
 
 // End of Server 2
-
+*/
 //-----------------------------------------------------------------------------------------
+
+// Remove lines 105 and 142 to use SERVER 3
 
 // Start of Server 3 Database configs
 
 // Server 3 - playerRanks - 3
 // Theses settings must match your playerRanks plugin config
-
+/*
 $servername_ply_rnk_3 = "localhost";
 $port_ply_rnk_3 = "3306";
 $username_ply_rnk_3 = "playerRanksdb3";
@@ -133,14 +139,16 @@ if ($conn_sql_stats_3->connect_error) {
 $srv3LastReset = "<b>Last stats reset time:</b> 08/20";
 
 // End of Server 3
-
+*/
 //-----------------------------------------------------------------------------------------
+
+// Remove lines 151 and 188 to use SERVER 4
 
 // Start of Server 4 Database configs
 
 // Server 4 - playerRanks - 4
 // Theses settings must match your playerRanks plugin config
-
+/*
 $servername_ply_rnk_4 = "localhost";
 $port_ply_rnk_4 = "3306";
 $username_ply_rnk_4 = "playerRanksdb4";
@@ -177,14 +185,16 @@ if ($conn_sql_stats_4->connect_error) {
 $srv4LastReset = "<b>Last stats reset time:</b> 08/20";
 
 // End of Server 4
-
+*/
 //-----------------------------------------------------------------------------------------
+
+// Remove lines 197 and 234 to use SERVER 5
 
 // Start of Server 5 Database configs
 
 // Server 5 - playerRanks - 5
 // Theses settings must match your playerRanks plugin config
-
+/*
 $servername_ply_rnk_5 = "localhost";
 $port_ply_rnk_5 = "3306";
 $username_ply_rnk_5 = "playerRanksdb5";
@@ -221,14 +231,16 @@ if ($conn_sql_stats_5->connect_error) {
 $srv5LastReset = "<b>Last stats reset time:</b> 08/20";
 
 // End of Server 5
-
+*/
 //-----------------------------------------------------------------------------------------
+
+// Remove lines 243 and 280 to use SERVER 2
 
 // Start of Server 6 Database configs
 
 // Server 6 - playerRanks - 6
 // Theses settings must match your playerRanks plugin config
-
+/*
 $servername_ply_rnk_6 = "localhost";
 $port_ply_rnk_6 = "3306";
 $username_ply_rnk_6 = "playerRanksdb6";
@@ -265,7 +277,7 @@ if ($conn_sql_stats_6->connect_error) {
 $srv6LastReset = "<b>Last stats reset time:</b> 08/20";
 
 // End of Server 6
-
+*/
 //-----------------------------------------------------------------------------------------
 
 $siteName = "Stats.com";
