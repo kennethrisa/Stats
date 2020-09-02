fd40d9144313c00a11d8d0b14ff54ff1fc09537d
P4R4NORM4L <belanger2008@gmail.com>
2020-09-02 11:31:55 -0600

    6 Server

--- navbar.php
+++ navbar.php
@@ -25,11 +25,11 @@ include("mconfig.php");
                 </li>
 		        <li><a href="<?php echo $siteURL; ?>">Home</a></li>
                 <li><a href="?id=1">Server 1</a></li>
-<!--            <li><a href="?id=2">Server 2</a></li>
-		        <li><a href="?id=3">Server 3</a></li>
-                <li><a href="?id=4">Server 4</a></li>
-                <li><a href="?id=5">Server 5</a></li>
-                <li><a href="?id=6">Server 6</a></li> -->
+<!--            <li><a href="?id=2">Server 2</a></li> -->
+<!--		<li><a href="?id=3">Server 3</a></li> -->
+<!--            <li><a href="?id=4">Server 4</a></li> -->
+<!--            <li><a href="?id=5">Server 5</a></li> -->
+<!--            <li><a href="?id=6">Server 6</a></li> -->
                 <!-- Uncomment above if you are gonna have multiple servers -->
             </ul>
         </div>
