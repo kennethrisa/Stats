ed385326119cc1dc2340daa2d0448e8102e98717
P4R4NORM4L <belanger2008@gmail.com>
2020-09-02 21:34:44 -0600

    Disable Scrolling-Nav feature

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,4 +1,5 @@
 //jQuery to collapse the navbar on scroll
+/*
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -6,7 +7,7 @@ $(window).scroll(function() {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
+*/
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
